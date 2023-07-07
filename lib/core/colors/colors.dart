81353f0b348660e5856d738593c6e398f75345f3
Nidhinlal <lalnidhinp02@gmai.com>
2023-07-07 14:02:19 +0530

    document page appbar ui completed

--- lib/core/colors/colors.dart
+++ lib/core/colors/colors.dart
@@ -2,3 +2,5 @@ import 'package:flutter/material.dart';
 
 const kWhiteColor = Colors.white;
 const kRedColor = Colors.redAccent;
+const kBlackColor = Colors.black;
+var kGreyColor = Colors.grey.shade800;
--- lib/featurs/document/screens/document_screen.dart
+++ lib/featurs/document/screens/document_screen.dart
@@ -1,4 +1,5 @@
 // ignore_for_file: public_member_api_docs, sort_constructors_first
+import 'package:doccs/core/colors/colors.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
@@ -13,9 +14,64 @@ class DocumentScreen extends ConsumerStatefulWidget {
 }
 
 class _DocumentScreenState extends ConsumerState<DocumentScreen> {
+  TextEditingController titelController =
+      TextEditingController(text: 'Untitled Document');
+
+  @override
+  void dispose() {
+    super.dispose();
+    titelController.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        title: Padding(
+          padding: const EdgeInsets.symmetric(vertical: 8.0),
+          child: Row(
+            children: [
+              Image.asset(
+                'assets/images/docs-logo.png',
+                height: 35,
+              ),
+              const SizedBox(width: 10),
+              SizedBox(
+                width: 180,
+                child: TextField(
+                  controller: titelController,
+                  decoration: const InputDecoration(
+                    border: InputBorder.none,
+                    focusedBorder: OutlineInputBorder(
+                        borderSide: BorderSide(
+                      color: Colors.purple,
+                    )),
+                    contentPadding: EdgeInsets.only(left: 10),
+                  ),
+                ),
+              )
+            ],
+          ),
+        ),
+        bottom: PreferredSize(
+          preferredSize: const Size.fromHeight(1),
+          child: Container(
+            decoration: BoxDecoration(
+              border: Border.all(color: kGreyColor, width: 0.1),
+            ),
+          ),
+        ),
+        actions: [
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: ElevatedButton.icon(
+              onPressed: () {},
+              label: const Text('Share'),
+              icon: const Icon(Icons.lock),
+            ),
+          ),
+        ],
+      ),
       body: Center(
         child: Text(widget.id),
       ),
